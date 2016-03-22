1cd0c028fa93b94e34d274f1c8cfc3e79fd7b980
Sarvex Jatasra <sarvex.jatasra@gmail.com>
2016-03-23 00:02:13 +0530

    added elpa

--- init.el
+++ init.el
@@ -92,6 +92,7 @@
 
 (require #'package)
 (setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("elpa" . "http://elpa.gnu.org/packages/") t)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
