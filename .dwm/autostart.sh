e4bf2d9d637a64e83b1050f5bc8bcf86164d680b
Derek Taylor <contact@unabot.com>
2019-02-01 17:31:23 -0600

    updating dwm status bar information.

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -3,7 +3,32 @@ compton --config ~/.config/compton/compton.conf &
 nitrogen --restore &
 urxvtd -q -o -f &
 
+dte(){
+  dte="$(date +"%A, %B %d - %H:%M")"
+  echo -e "🕒 $dte"
+}
+
+upd(){
+  upd=`checkupdates | wc -l`
+  echo -e "⟳ $upd updates"
+}
+
+mem(){
+  mem=`free | awk '/Mem/ {printf "%d MiB/%d MiB\n", $3 / 1024.0, $2 / 1024.0 }'`
+  echo -e "🖪 $mem"
+}
+
+cpu(){
+  read cpu a b c previdle rest < /proc/stat
+  prevtotal=$((a+b+c+previdle))
+  sleep 0.5
+  read cpu a b c idle rest < /proc/stat
+  total=$((a+b+c+idle))
+  cpu=$((100*( (total-prevtotal) - (idle-previdle) ) / (total-prevtotal) ))
+  echo -e "💻 $cpu% cpu"
+}
+
 while true; do
-    xsetroot -name "` date +"%b %e, %Y - %R" `"
-    sleep 1m    # Update time every minute
+     xsetroot -name "$(cpu) | $(mem) | $(upd) | $(dte)"
+     sleep 10s    # Update time every ten seconds
 done &
