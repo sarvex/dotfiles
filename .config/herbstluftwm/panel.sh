a8e80e364635cc39e191be3b7172f90528c6beab
Derek Taylor <derek@distrotube.com>
2020-07-24 15:15:57 -0500

    Working on herbstluftwm.

--- .config/herbstluftwm/panel.sh
+++ .config/herbstluftwm/panel.sh
@@ -8,10 +8,10 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload mainbar-xmonad &
+    MONITOR=$m polybar --reload mainbar-herbst &
   done
 else
-  polybar --reload mainbar-xmonad &
+  polybar --reload mainbar-herbst &
 fi
 
 echo "Bars launched..."
