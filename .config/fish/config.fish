f8099ee0579550bf106a2dbbb13f3acb912ad568
Derek Taylor <derek@distrotube.com>
2020-11-13 22:11:01 -0600

    Adding thumb for st-distrotube

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -109,8 +109,8 @@ end
 
 ### DEFAULT EMACS MODE OR VI MODE ###
 function fish_user_key_bindings
-  fish_default_key_bindings
-  # fish_vi_key_bindings
+  # fish_default_key_bindings
+  fish_vi_key_bindings
 end
 ### END OF VI MODE ###
 
