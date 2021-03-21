585823b37466046cdf605ab087521a0a2a0d42d0
Derek Taylor <derek@distrotube.com>
2021-03-20 21:57:45 -0500

    Cleaning up Doom.

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -35,7 +35,7 @@
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
        ;;indent-guides     ; highlighted indent columns
-       ;;ligatures         ; ligatures and symbols to make your code pretty again
+       (ligatures +extras)         ; ligatures and symbols to make your code pretty again
        ;;minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
        ;;nav-flash         ; blink cursor line after big motions
@@ -179,14 +179,8 @@
        emms
        ;;everywhere        ; *leave* Emacs!? You must be joking
        ;;irc               ; how neckbeards socialize
-       ;;(rss +org)        ; emacs as an RSS reader
+       (rss +org)        ; emacs as an RSS reader
        ;;twitter           ; twitter client https://twitter.com/vnought
-
-       ;; :emms
-       ;; emms-setup
-       ;; emms-info
-       ;; emms-cue
-       ;; emms-mode-line
 
        :config
        literate
