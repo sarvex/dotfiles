9c262470a4d5b0a2f513b3dfcf139090c9be7426
Derek Taylor <derek@distrotube.com>
2021-04-16 00:06:44 -0500

    GNU Emacs config from video: Leaving Doom For GNU Emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,3 +3,16 @@
   "config.org"
   user-emacs-directory))
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(all-the-icons-dired which-key vterm use-package toc-org projectile peep-dired org-bullets magit-todos magit-lfs ivy-rich ivy-posframe haskell-mode general evil-collection eshell-syntax-highlighting doom-themes doom-modeline dired-open dashboard counsel)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
