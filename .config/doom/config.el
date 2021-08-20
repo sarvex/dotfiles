2f328fc0f592482488c23dbbfcf25446053cf157
Derek Taylor <derek@distrotube.com>
2021-08-20 17:09:21 -0500

    Adding org-publish to Doom Emacs.

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -433,6 +433,24 @@
 
 (use-package ox-man)
 (use-package ox-gemini)
+(use-package ox-publish)
+
+(setq org-publish-project-alist
+      '(("org-notes"
+         :base-directory "~/Org/website"
+         :base-extension "org"
+         :publishing-directory "~/public_html/"
+         :recursive t
+         :publishing-function org-html-publish-to-html
+         :headline-levels 4             ; Just the default for this project.
+         :auto-preamble t)
+         ("org-static"
+         :base-directory "~/Org/website"
+         :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
+         :publishing-directory "~/public_html/"
+         :recursive t
+         :publishing-function org-publish-attachment)
+      ))
 
 (use-package! password-store)
 
