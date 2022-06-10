404d307e3dd2c97682902157c47a4adcc659dc7d
Derek Taylor <derek@distrotube.com>
2022-06-10 13:02:31 -0500

    Minor edits to Doom configs.

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -237,7 +237,7 @@ List of keybindings (SPC h b b)")
       erc-nick "distrotube"
       erc-user-full-name "Derek Taylor"
       erc-track-shorten-start 24
-      erc-autojoin-channels-alist '(("irc.libera.chat" "#unixtube" "#distrotube"))
+      erc-autojoin-channels-alist '(("irc.libera.chat" "#archlinux" "#linux" "#emacs"))
       erc-kill-buffer-on-part t
       erc-fill-column 100
       erc-fill-function 'erc-fill-static
